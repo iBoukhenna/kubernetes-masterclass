1c117eecad0e42a4ddadd6448b87a08545e943f2
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-07-28 12:07:45 -0400

    add getServerSideProps to get data

--- nextjs-project/pages/bookmarks/index.tsx
+++ nextjs-project/pages/bookmarks/index.tsx
@@ -1,8 +1,21 @@
-export default function Home() {
+import { GetServerSideProps } from "next";
+
+interface HomeProps {
+  titles: string[];
+}
+export default function Home({titles}: HomeProps) {
     return (
       <div>
           <h1>Bookmarks</h1>
+          { titles.map(title => <h2>{title}</h2>) }
       </div>
     )
 }
-  
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: { 
+      titles : ['1', '2', '3']
+    }
+  }
+}
