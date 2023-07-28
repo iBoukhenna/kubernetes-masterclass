1bb36c5572bd3abc41c93b3d3f77a8907660f657
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-07-27 22:41:12 -0400

    react router to navigate between diffrent pages

--- reactjs-project/src/App.tsx
+++ reactjs-project/src/App.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import User from './User';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      <User name={"John"} email={"John@gmail.com"}/>
-      <User name={"Amine"} email={"Amine@gmail.com"}/>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
