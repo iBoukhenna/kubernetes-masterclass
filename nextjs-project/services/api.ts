879e44a761bae32174f48d8eb32293b9828261f6
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-07-28 20:18:01 -0400

    dockerizing nextjs application

--- nextjs-project/services/api.ts
+++ nextjs-project/services/api.ts
@@ -1,13 +1,22 @@
-import axios, { AxiosResponse } from "axios"
+import axios from "axios"
 import { BookmarksResponse } from "./models";
+import getConfig from 'next/config'
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
-const API_BASE_URL = "http://localhost:8080"
+const getApiUrl = () => {
+    return serverRuntimeConfig.API_BASE_URL || publicRuntimeConfig.API_BASE_URL;
+}
+
 export const fetchBookmarks = async (page: number, query: string): Promise<BookmarksResponse> => {
-  const res = await axios.get<BookmarksResponse>(`${API_BASE_URL}/api/bookmarks?page=${page}&query=${query}`)
+  let url = `${getApiUrl()}/api/bookmarks?page=${page}`
+  if (query) {
+    url += `&query=${query}`
+  }
+  const res = await axios.get<BookmarksResponse>(url)
   return res.data
 }
 
 export const saveBookmark = async (bookmark: {title: string, url: string}) => {
-  const res = await axios.post(`${API_BASE_URL}/api/bookmarks`, bookmark)
+  const res = await axios.post(`${getApiUrl()}/api/bookmarks`, bookmark)
   return res.data
 }
